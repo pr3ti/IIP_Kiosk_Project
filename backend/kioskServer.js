13fded6ad963b4476070090fd3079c02b583d03e
pr3ti <23011120@myrp.edu.sg>
2026-02-02 09:38:59 +0800

    Update kioskServer.js 10mb to 50mb

--- backend/kioskServer.js
+++ backend/kioskServer.js
@@ -200,8 +200,8 @@ if (fs.existsSync(certPath) && fs.existsSync(keyPath)) {
 
 // ==================== MIDDLEWARE CONFIGURATION ====================
 
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 app.use(
   session({
